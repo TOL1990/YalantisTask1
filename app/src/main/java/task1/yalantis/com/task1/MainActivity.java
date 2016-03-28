c6fe0d43b4c2388cfde8e20fb356c54956417584
TOL1990 <olefir.taras@mail.ru>
2016-03-28 12:22:05 +0300

    Initial commit

---
+++ app/src/main/java/task1/yalantis/com/task1/MainActivity.java
@@ -0,0 +1,44 @@
+package task1.yalantis.com.task1;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadImages();
+
+    }
+
+    private void loadImages() {
+        //Reload old vers.
+        ImageView img1 = (ImageView) findViewById(R.id.image1);
+        ImageView img2 = (ImageView) findViewById(R.id.image2);
+        Picasso.with(this).load("http://savepic.ru/9117399.png").into(img1);
+        Picasso.with(this).load("http://savepic.ru/9172695.png").into(img2);
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+
+        Toast.makeText(this, "Any control", Toast.LENGTH_SHORT).show();
+
+//       System.exit(0);
+    }
+
+
+}
